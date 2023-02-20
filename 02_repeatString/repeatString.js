3d16da7723f0c2c33e12d27f3404ad475db53774
Maarten Huisman <mrtn.hsmn@gmail.com>
2023-02-20 22:26:50 +0100

    do exercise 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    for (num; num > 0; num--) {
+        let newString = string+string
+        return newString
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
