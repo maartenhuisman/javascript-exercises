4678b642dc8fc11ac464936bcbc246429793e8af
Maarten Huisman <mrtn.hsmn@gmail.com>
2023-02-21 22:00:08 +0100

    Do excercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(totalArray, others) {
+    const othersArray = [others];
+    let othersLength = othersArray.length;
+    let totalLength = totalArray.length;
+    console.log(totalArray);
+    console.log(othersArray);
+    console.log(othersLength);
+    for (let i = 0; i < othersLength; i++) {
+        for (let i = 0; i < totalLength; i++) {
+            if (totalArray[i] === othersArray[0]) {
+                totalArray.splice(i,1);
+            }
+            console.log(totalArray);
+        }
+    }
+    return totalArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
