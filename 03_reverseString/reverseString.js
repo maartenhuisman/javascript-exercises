743af27697eff80b33d0ca9f27326ede295c942c
Maarten Huisman <mrtn.hsmn@gmail.com>
2023-02-21 20:06:56 +0100

    Complete exercise 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textArray = text.split('');
+    let reversedArray = textArray.reverse();
+    let reversedText = reversedArray.join('');
+    return reversedText;
 };
 
 // Do not edit below this line
