511daac5c93750dc675ba699984f390be4cf5c8e
Maarten Huisman <mrtn.hsmn@gmail.com>
2023-02-27 18:51:33 +0100

    Clean up completed exercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,8 @@
 const sumAll = function(number1, number2) {
     let sum = 0
-    if ((number1 < 0 || number2 < 0)) {
+    if (number1 < 0 || number2 < 0) {
         return 'ERROR';
-    } else if ((!(Number.isInteger(number1)) || !(Number.isInteger(number2)))) {
+    } else if (!Number.isInteger(number1) || !Number.isInteger(number2)) {
         return 'ERROR';
     } else {
         if (number1 < number2) {
